cd8df12d45f99ef55f8d81d01bf24b9bd87a1fbd
Desewah <adesewamoyofade@gmail.com>
2023-01-25 02:21:55 +0100

    update on my portfolio

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Frontend Developer"],
+        strings: ["Software Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Frontend Developer"],
+        strings: ["Software Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
